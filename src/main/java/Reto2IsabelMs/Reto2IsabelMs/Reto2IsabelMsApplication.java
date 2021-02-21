2766d533953422778513329761a8de8dd13a2513
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2021-02-21 18:20:18 +0100

    commit1

---
+++ src/main/java/Reto2IsabelMs/Reto2IsabelMs/Reto2IsabelMsApplication.java
@@ -0,0 +1,13 @@
+package Reto2IsabelMs.Reto2IsabelMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2IsabelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2IsabelMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Reto2IsabelMs/Reto2IsabelMs/Reto2IsabelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto2IsabelMs.Reto2IsabelMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2IsabelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
